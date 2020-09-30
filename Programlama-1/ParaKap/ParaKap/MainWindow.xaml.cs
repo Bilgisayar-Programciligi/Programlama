307278906c44b3aa5e931c47856fa164ce19e277
Şükrü Berkay(bipro) <biproberkay@outlook.com>
2020-10-01 02:00:32 +0300

    Proje repoya kopyalandı. çalıştırma esnasında sqlite ile ilgili hata alındı

---
+++ Programlama-1/ParaKap/ParaKap/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ParaKap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow() //Yapıcı Method
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Random r = new Random();
+            Para p = new Para(this);
+            cOyunAlani.Children.Add(p);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.IsEnabled = !timer.IsEnabled;
+        }
+    }
+}
